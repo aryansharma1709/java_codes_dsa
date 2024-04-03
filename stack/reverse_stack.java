1042ade69688e975a9c89150b809cbbd90d044d4
Aryan Sharma <aryansharmag29@gmail.com>
2024-04-03 12:35:23 +0530

    stacks reversed

---
+++ stack/reverse_stack.java
@@ -0,0 +1,36 @@
+package stack;
+import java.util.*;
+public class reverse_stack {
+
+        public static void main(String[] args) {
+            Stack<Integer> st=new Stack<>();
+            st.push(10);
+            st.push(20);
+            st.push(30);
+            st.push(40);
+            st.push(50);
+            System.out.println(st);
+            reverse(st);
+            System.out.println(st);
+        }
+        public static void reverse(Stack<Integer> st){
+            if(st.isEmpty())
+            {
+                return;
+            }
+            int x=st.pop();
+            reverse(st);
+            Insert(st,x);
+        }
+        public static void Insert(Stack<Integer> st,int item){
+            if(st.isEmpty())
+            {
+                st.push(item);
+                return;
+            }
+            int x=st.pop();
+            Insert(st,item);
+            st.push(x);
+        }
+    }
+
