8e104e2af0d46de6a9044ab0001bbf5ed4bb1306
Aryan Sharma <aryansharmag29@gmail.com>
2024-04-04 12:11:04 +0530

    stack problem

--- stack/find_the_celebrity.java
+++ stack/find_the_celebrity.java
@@ -1,38 +1,38 @@
 package stack;
 import java.util.*;
 public class find_the_celebrity {
-//    indian idol algorithm (elemination algorithm) stack problems
+    //    indian idol algorithm (elemination algorithm) stack problems
     public static void main(String[] args) {
         int [][] graph={{0,1,1,1},{1,0,1,1},{0,0,0,0},{1,1,1,0}};
-         System.out.println(celebrity(graph));
+        System.out.println(celebrity(graph));
     }
     public static int celebrity(int [] []graph){
-     Stack<Integer> st= new Stack<>();
-     for(int i=0;i<graph.length;i++)
-     {
-         st.push(i);
-     }
-     while(st.size()>1)
-     {
-         int a=st.pop();
-         int b=st.pop();
-         if(graph[a][b]==1){
-             st.push(b);
-         }
-         else{
-             st.push(a);
-         }
-     }
-     int candidate=st.pop();
-     for(int i=0;i< graph.length;i++)
-     {
-         if(i==candidate)
-             continue;
-         if(graph[candidate][i]==1 || graph[i][candidate]==0)
-         {
-             return-1;
-         }
-     }
-     return candidate;
+        Stack<Integer> st= new Stack<>();
+        for(int i=0;i<graph.length;i++)
+        {
+            st.push(i);
+        }
+        while(st.size()>1)
+        {
+            int a=st.pop();
+            int b=st.pop();
+            if(graph[a][b]==1){
+                st.push(b);
+            }
+            else{
+                st.push(a);
+            }
+        }
+        int candidate=st.pop();
+        for(int i=0;i< graph.length;i++)
+        {
+            if(i==candidate)
+                continue;
+            if(graph[candidate][i]==1 || graph[i][candidate]==0)
+            {
+                return-1;
+            }
+        }
+        return candidate;
     }
 }
