9dd3182524957f63f98e0e23d4da23d75a5dabd8
Aryan Sharma <aryansharmag29@gmail.com>
2024-04-04 12:04:00 +0530

    stack problem

--- stack/find_the_celebrity.java
+++ stack/find_the_celebrity.java
@@ -1,7 +1,7 @@
 package stack;
 import java.util.*;
 public class find_the_celebrity {
-//    indian idol algorithm (elemination algorithm)
+//    indian idol algorithm (elemination algorithm) stack problem
     public static void main(String[] args) {
         int [][] graph={{0,1,1,1},{1,0,1,1},{0,0,0,0},{1,1,1,0}};
          System.out.println(celebrity(graph));
