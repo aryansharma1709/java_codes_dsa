a83ff1b2fccfc14309518ef4ed0ac1097b5c0a08
aryansharma <aryansharmag29@gmail.com>
2024-06-26 20:14:13 +0530

    methods updated

--- Striver_Linked_list/linked_list.java
+++ Striver_Linked_list/linked_list.java
@@ -1,4 +1,5 @@
 package Striver_Linked_list;
+//--------------------------------------------------------------------------------------
 class node{
     int data;
     node next;
@@ -8,7 +9,9 @@ class node{
         this.next=null;
     }
 }
+//----------------------------------------------------------------------------------------------------------
 public class linked_list {
+//-----------------convert the array into linkedlist-------------------------
     private static node convertArr2ll(int []arr){
         node head=new node(arr[0]);
         node mover=head;
@@ -22,6 +25,9 @@ public class linked_list {
         }
         return head;
     }
+//------------------------------------------------------------------------------------------
+
+//----------------length of the linked List-----------------------------------------
     private static int lengthNode(node head)
     {
         int count=0;
@@ -33,6 +39,9 @@ public class linked_list {
         }
     return count;
     }
+//-------------------------------------------------------------------------------
+
+//---------------------------element present or not in the linked list-----------------------------
  private static boolean elementPresnt(node head,int val){
      node temp=head;
      while(temp!=null)
@@ -43,18 +52,92 @@ public class linked_list {
      }
      return false;
  }
+// ----------------------------------------------------------------------------
 
+//----------------------------remove the head of the linked list--------------------
+ private static node removeHead(node head)
+ {
+     if(head==null)return head;
+     head=head.next;
+     return head;
+ }
+// ----------------------------------------------------------------------------
+
+//------------------------remove the tail of the linked list-------------------------------
+ private static node removeTail(node head)
+ {
+     if(head==null || head.next==null)
+     {
+         return null;
+     }
+     node temp=head;
+     while(temp.next.next!=null)
+     {
+         temp=temp.next;
+     }
+     temp.next=null;
+     return head;
+ }
+// ---------------------------------------------------------------------------------------------
+
+//-----------------------------------delete the kth element of the linked list--------------------
+    private static  node delete_kth_element(node head,int k)
+    {
+       if(k==1) return removeHead(head);
+       else if(k==lengthNode(head)) return removeTail(head);
+       else if(k>lengthNode(head)) return head;
+
+           int c=1;
+           node temp=head;
+           while(c<k-1)
+           {
+               temp=temp.next;
+               c++;
+           }
+           temp.next=temp.next.next;
+      return head;
+    }
+// -----------------------------------------------------------------------------------------------------
+
+//-----------------------------------element deleted by its values---------------------------
+    private static node delete_by_value(node head ,int val)
+    {
+       if(head==null)return head;
+        else if(head.data==val)return head;
+        node temp=head;
+        node prev=null;
+        while(temp!=null) {
+            if (temp.data == val && temp.next == null) {
+                prev.next = null;
+                break;
+            } else if (temp.data == val) {
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+//------------------------------------------------------------------------------------------
+//------------------------------MAIN-----------------------------------------------------
     public static void main(String[] args) {
      int [] arr={12,5,6,3,4};
      node head=convertArr2ll(arr);
-//     System.out.println(head.next);
-     System.out.println(lengthNode(head));
-     node temp=head;
-     System.out.println(elementPresnt(head,15));
+//     System.out.println(head.data);
+//        head=removeHead(head);
+//     System.out.println(head.data);
+//     System.out.println(lengthNode(head));
+//        head=removeTail(head);
+//        head=delete_kth_element(head,4);
+//        head=delete_by_value(head,3);
+       node temp=head;
+//     System.out.println(elementPresnt(head,15));
      while(temp!=null)
      {
-//         System.out.println(temp.data);
+         System.out.println(temp.data);
          temp=temp.next;
      }
     }
+//---------------------------------------------------------------------
 }
